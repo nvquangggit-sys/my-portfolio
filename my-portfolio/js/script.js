98e172856ea8173866f55e6aab0c1f81e1d69e21
nvquangggit-sys <nvquang.ggit@gmail.com>
2026-03-30 13:57:00 +0700

    Delete my-portfolio directory

--- my-portfolio/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Nguyễn Việt Quang đã  load xong!');
-
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
-});
